f7fe73886154bd0668f7cbf257a877af19357825
yongchao71 <408775927@qq.com>
2018-03-23 19:49:20 +0800

    change style

---
+++ config/constant.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: ZXY 
+ * @Date: 2018-03-23 09:11:26 
+ * @Last Modified by: ZXY
+ * @Last Modified time: 2018-03-23 09:37:43
+ */
+
+ module.exports={
+     errorCode:{
+         paramsError:{
+             code:600,message:"参数错误"
+         },
+         requestError:{
+            code:700,message:"请求异常"
+         }
+     }
+ }
--- utils/qPromise.js
+++ utils/qPromise.js
@@ -2,19 +2,24 @@
  * @Author: ZXY 
  * @Date: 2018-03-21 09:14:53 
  * @Last Modified by: ZXY
- * @Last Modified time: 2018-03-21 11:15:00
+ * @Last Modified time: 2018-03-23 14:34:09
  */
 
-var Q = require('q');
-var request = require('request');
-const util = require('util');
+var Q = require("q");
+var request = require("request");
+var loger=request("./loger.js");
+var cUtils=require("./cUtils");
+var CONSTANT=require("../config/constant");
+const util = require("util");
 function requestUrl(url) {
     return Q.Promise(function(resolve, reject, notify) {
         request(url, function (error, response, body) {
-            if(error)
+            if(error){
                 reject(error);
-            if (!error && response.statusCode == 200) {
-                resolve(body);
+            }else{
+                if (!error && response.statusCode == 200) {
+                    resolve(body);
+                }
             }
         })
     });
@@ -23,16 +28,54 @@ function createPromise(url){
     var deferred = Q.defer();
     request(url , function(err , response , body){
         console.log("requested "+url);
-        if(err)
+        if(err){
             deferred.reject(err);
-        else
+        }        
+        else{
             deferred.resolve(body);
+        }
     });
     return deferred.promise;
 }
-function qPromise(){
+function qPromise(oParams={}){
     var deferred = Q.defer();
+    let options={
+        //url:oParams.url,
+        method:"POST", 
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        },
+       //body:JSON.stringify(arg.data)
+    };
+    cUtils.extend(options,oParams);
+    loger.info(options.url,options.body);
+    request(options , function(error , response , body){
+        if(error){
+            deferred.resolve(error);
+        }
+        else{
+            deferred.resolve(body);
+        }
+    });
+    return deferred.promise;
+
 };
+
+function wxPromise(fn) {
+    return function (obj = {}) {
+      return new Promise((resolve, reject) => {
+        obj.success = function (res) {
+          resolve(res);
+        }
+        obj.fail = function (res) {
+          //reject(res);
+          resolve(res);
+        }
+        fn(obj);
+      })
+    }
+  };
+
 module.exports={
     qPromise:qPromise
 }
