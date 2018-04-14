79133dcc614b3796b5120395ea58148ed4ceefe4
yongchao71 <408775927@qq.com>
2018-04-15 01:35:57 +0800

    change request

--- config/remoteAPI.js
+++ config/remoteAPI.js
@@ -2,19 +2,25 @@
  * @Author: ZXY 
  * @Date: 2018-04-14 14:04:43 
  * @Last Modified by: ZXY
- * @Last Modified time: 2018-04-14 17:20:04
+ * @Last Modified time: 2018-04-14 23:12:14
  */
 const bootConfig=require('./bootConfig.json'); //
 const bootenv=bootConfig.bootenv||bootConfig.defaultenv; //bootenv
 let remoteHost={
-        dev:"https://sit-api.nihaomc.com",
+        dev:{
+          mserver:"https://sit-api.nihaomc.com",
+          local:`http://localhost:${bootConfig.serverport}`
+        },
         sit:"https://sit-api.nihaomc.com",
         uat:"https://uat-api.nihaomc.com",
         prod:"https://prod-api.nihaomc.com"
     }
 var config={
     product: {
-      list:`${remoteHost[bootenv]}/api/product/list`
+      list:`${remoteHost[bootenv].mserver}/api/product/list`
+    },
+    users:{
+      add:`${remoteHost[bootenv].local}/users/add`
     }
 }
 module.exports=config;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,9 +2,9 @@
  * @Author: ZXY 
  * @Date: 2018-03-20 13:41:18 
  * @Last Modified by: ZXY
- * @Last Modified time: 2018-04-14 19:42:03
+ * @Last Modified time: 2018-04-15 01:32:37
  */
-var product=require("./../config/remoteAPI").product;
+var CONFIGAPI=require("./../config/remoteAPI");
 var httpRequest=require("./../utils/httpRequest");
 var loger=require("../utils/loger").loger();
 
@@ -18,25 +18,36 @@ function detail(req, res, next){
 function add(req, res, next){
     let aaa=23;
     loger.info("add-----",req.session);
+    loger.info(req.body);
     req.session.user=req.query.uname||"00";
     res.send({"hhhhhh":"hjgjhgdfgh枯干古jhg"});
 }
 function test(req,res,next){
-    loger.info("product---------",product);
+
+    let getUrl=`${CONFIGAPI.product.list}?unionid=oDOgS0kCV5its31fROZtbdqcpMAE&test=測試`;
+
+    let pdata={unionid:"oDOgS0kCV5its31fROZtbdqcpMAE",test:"測試"};
+    let getUrl2=CONFIGAPI.product.list;
+    let postUrl=`${CONFIGAPI.users.add}?unionid=oDOgS0kCV5its31fROZtbdqcpMAE&test=測試`;
     let oParams={
-        url:`${product.list}?unionid=oDOgS0kCV5its31fROZtbdqcpMAE`,
-        method:"get",
+        url:getUrl,
+        url:postUrl,
+        method:"POST",
         body:{
             unionid:"oDOgS0kCV5its31fROZtbdqcpMAE",
-            test:"sdfgsdfg"
+            AAA:"sdfgsdfg"
         }
     };
-    httpRequest.Request(oParams).then(result=>{
+    // httpRequest.Request(oParams).then(result=>{
+    //     console.log("result-------------",typeof(result),Array.isArray(result),result.length);
+    //     console.log("result-------------",result[1]);
+    //     res.send(result[1]);
+    // });
+    httpRequest.Request(postUrl,"post",pdata).then(result=>{
         console.log("result-------------",typeof(result),Array.isArray(result),result.length);
         console.log("result-------------",result[1]);
         res.send(result[1]);
     });
-
     //res.send("this is test data..");
 }
 module.exports={
