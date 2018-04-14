a420943582b5dbbc734f5ce9960b84904c43e893
yongchao71 <408775927@qq.com>
2018-04-14 19:59:15 +0800

    change url

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,10 +2,12 @@
  * @Author: ZXY 
  * @Date: 2018-03-20 13:41:18 
  * @Last Modified by: ZXY
- * @Last Modified time: 2018-03-20 16:32:09
+ * @Last Modified time: 2018-04-14 19:42:03
  */
-
+var product=require("./../config/remoteAPI").product;
+var httpRequest=require("./../utils/httpRequest");
 var loger=require("../utils/loger").loger();
+
 function detail(req, res, next){
     let aaa=23;
     loger.info("detail-----",req.session);
@@ -19,7 +21,26 @@ function add(req, res, next){
     req.session.user=req.query.uname||"00";
     res.send({"hhhhhh":"hjgjhgdfgh枯干古jhg"});
 }
+function test(req,res,next){
+    loger.info("product---------",product);
+    let oParams={
+        url:`${product.list}?unionid=oDOgS0kCV5its31fROZtbdqcpMAE`,
+        method:"get",
+        body:{
+            unionid:"oDOgS0kCV5its31fROZtbdqcpMAE",
+            test:"sdfgsdfg"
+        }
+    };
+    httpRequest.Request(oParams).then(result=>{
+        console.log("result-------------",typeof(result),Array.isArray(result),result.length);
+        console.log("result-------------",result[1]);
+        res.send(result[1]);
+    });
+
+    //res.send("this is test data..");
+}
 module.exports={
     detail:detail,
-    add:add
+    add:add,
+    test:test
 }
--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,9 @@ var router = express.Router();
 var usersController=require("../controllers/usersController");
 router.get('/detail', usersController.detail);
 router.get('/add', usersController.add);
+router.get('/test', usersController.test);
+router.get('/get', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
