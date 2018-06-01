9b2039fae744ae909fb89b1fac13a98885976c69
yongchao71 <408775927@qq.com>
2018-06-01 22:09:48 +0800

    change node frame

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,14 @@
  * @Author: ZXY 
  * @Date: 2018-06-01 15:45:26 
  * @Last Modified by: ZXY
- * @Last Modified time: 2018-06-01 16:00:04
+ * @Last Modified time: 2018-06-01 22:04:39
  */
 
 var express = require('express');
 var router = express.Router();
 var usersController=require("../controllers/userController");
 router.get('/detail', usersController.detail);
+router.get('/list', usersController.list);
 router.post('/add', usersController.add);
 router.get('/test', usersController.test);
 router.get('/get', function(req, res, next) {
