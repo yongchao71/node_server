ae8e3510d4640d6efef1121751c8f3714d38cc8b
yongchao71 <408775927@qq.com>
2018-06-01 22:30:45 +0800

    change node frame

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,18 @@
  * @Author: ZXY 
  * @Date: 2018-03-20 13:41:18 
  * @Last Modified by: ZXY
- * @Last Modified time: 2018-06-01 22:07:58
+ * @Last Modified time: 2018-06-01 22:15:40
  */
+var express = require('express');
+var router = express.Router();
 var CONFIGAPI = require("./../config/remoteAPI");
 var httpRequest = require("./../utils/httpRequest");
 var userService=require("./../service/userService");
 var loger = require("../utils/loger").loger();
 
-function detail(req, res, next) {
+
+
+router.get('/detail',(req, res, next)=> {
     userService.detail().then(result => {
         loger.info("Users-------------->", JSON.stringify(result));
         res.send(result);
@@ -17,23 +21,21 @@ function detail(req, res, next) {
        // loger.error("e-------------->", e);
         res.send({ "error": "load error" });
     });
-}
-function list(req, res, next) {
+});
+router.get('/list', (req, res, next)=>{
     userService.list().then(result => {
         loger.info("Users---list----------->", JSON.stringify(result));
         res.send(result);
     })
-}
-function add(req, res, next) {
+});
+router.post('/add',(req, res, next)=>{
 
-}
+});
+router.get('/test', (req, res, next)=>{
 
-function test(req, res, next) {
+});
+router.get('/get', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 
-}
-module.exports = {
-    detail: detail,
-    add: add,
-    list:list,
-    test: test
-}
+module.exports = router;
